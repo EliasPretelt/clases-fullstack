8264a671b73364db334cefabfe6cf78278dcfbc0
EliasPretelt <66378392+EliasPretelt@users.noreply.github.com>
2020-08-26 08:20:34 -0500

    Add files via upload

---
+++ 08-25-2020/jsprof.js
@@ -0,0 +1,124 @@
+/* Metodo para mostrar mensajes */
+function mensajeAlert() {
+    alert("Hola mundooo!!!");
+}
+
+/* Prompt */
+function cajitaDeTexto() {
+    var nombre= prompt('Ingrese su nombre');
+    document.write("<h1> Su nombre es " + nombre + "</h1>")
+}
+
+/* Confirm */
+
+function confirmar() {
+    var respuesta= confirm('Esta seguro que dese eliminar el archivo?');
+
+    if (respuesta) {
+       alert("Archivo eliminado correctamente!!");
+    } else {
+       alert("Archivo NO eliminado"); 
+    }
+}
+
+function consola() {
+    console.log("Soy tu mejor amigo para los errores");
+}
+
+/* imagen.addEventListener("click", funtion(){
+
+}) */
+var imagen= document.getElementById("imagen"); 
+
+imagen.addEventListener("dblclick", ()=>{
+    /* alert("Soy el DOM"); */
+    /* Modificamos el CSS */
+    imagen.style.width= "1000px"
+    imagen.style.border= "5px solid black"
+})
+
+/* Mouse */
+
+var boxCaja= document.getElementById("box");
+
+/* document.getElementById("box").addEventListener("mouseenter", ()=>{
+ box.classList.replace("box","cambio1")
+}) */
+
+/* mouseleave */
+
+boxCaja.addEventListener("mouseenter",function(){
+ 
+    boxCaja.classList.replace("boxcam","cambio1")
+
+})
+
+boxCaja.addEventListener("mouseleave",function(){
+    
+    boxCaja.classList.replace("cambio1","boxcam")
+
+})
+
+boxCaja.addEventListener("mousemove",()=>{
+    console.log("El mouse se esta moviendo")
+})
+
+boxCaja.addEventListener("mousedown", ()=>{
+    console.log("Has dado click dentro del div")
+})
+boxCaja.addEventListener("mouseup", ()=>{
+    console.log("Has levantado el click dentro del div")
+})
+
+
+/* TECLADO */
+
+var teclado= document.getElementById("teclado");
+
+/* teclado.addEventListener("keydown", ()=>{
+    console.log("Has oprimido una tecla")
+})
+teclado.addEventListener("keypress", ()=>{
+    alert("Tienes la tecla sostenida");
+})  */
+
+teclado.addEventListener("keyup", (e)=>{
+     console.log(e.key)
+
+
+     if (e.key.toLowerCase() == "w" ){
+         alert("Vas hacia adelante")
+     }
+})
+
+
+/* Objeto evento */
+
+var padre= document.getElementById("padre");
+
+padre.addEventListener("click", (e)=>{
+    console.log(e.target.textContent)
+    /* e.target.style.background="red" */
+
+ e.target.classList.add("color")
+})
+
+/* CREAR ELEMENTOS DENTRO DE UN NODO O ETIQUETA */
+
+var lista= document.getElementById("lista");
+var agregar= document.getElementById("agregar")
+
+agregar.addEventListener("click", ()=>{
+ var texto= document.getElementById("texto").value;
+ /* Me trae el texto o informacion ingresado dentro del input */
+
+ var etiqueta= document.createElement("li");
+ /* CREO LA ETIQUETA */
+
+ etiqueta.textContent= texto;
+ /* ASIGNAMOS EL VALOR.DENTRO DE LA ETIQUE INSERTO EL TEXTO QUE TIENE EL INPUT */
+
+    lista.appendChild(etiqueta)
+    /* AGREGAMOS LA ETIQUETA A LA LISTA O NODO */
+
+})
---
+++ 08-25-2020/script.js
@@ -0,0 +1,139 @@
+//alert("hola todos");
+
+function mensajeAlert() {
+    alert("hola todos");
+    
+}
+
+function boxtext() {
+    var nombre= prompt('ingrese su nombre');
+    document.write("<h1> su nombre es" + nombre + "</h1>");
+    
+}
+
+function confitmar() {
+    var respuesta=confirm("quiere eliminar?");
+
+    if (respuesta == true){
+        alert("archivo eliminado");
+    }else{
+        alert("archivo no eliminado");
+
+    }    
+      
+}
+
+
+function consola() {
+    console.log("soy tu amigo para los errores");
+   
+}
+var imagen= document.getElementById("img");
+
+imagen.addEventListener("dblclick", ()=>{
+    imagen.style.width="900px"
+    imagen.style.border="5px solid black"
+
+})
+
+
+/* Mouse */
+
+var boxCaja= document.getElementById("box");
+
+/* document.getElementById("box").addEventListener("mouseenter", ()=>{
+ box.classList.replace("box","cambio1")
+}) */
+
+/* mouseleave */
+
+boxCaja.addEventListener("mouseenter",function(){
+ 
+    boxCaja.classList.replace("box","cambio1")
+
+})
+
+boxCaja.addEventListener("mouseleave",function(){
+ /* El primer parametro es la clase existen o actual y el segundo parametro es la que lo va a remplazar  */
+    boxCaja.classList.replace("cambio1","box")
+
+})
+
+boxCaja.addEventListener("mousemove",()=>{
+    console.log("El mouse se esta moviendo")
+})
+
+boxCaja.addEventListener("mousedown", ()=>{
+    console.log("Has dado click dentro del div")
+})
+boxCaja.addEventListener("mouseup", ()=>{
+    console.log("Has levantado el click dentro del div")
+})
+
+
+var teclado=document.getElementById("teclado");
+
+/*teclado.addEventListener("keydown", ()=>{
+    console.log("tocaste una tecla");
+})
+
+teclado.addEventListener("keypress", ()=>{
+    console.log("tienes totsh una tecla");
+})*/
+
+teclado.addEventListener("keyup", (e)=>{
+    console.log(e.key );
+    
+    if (e.key.toLowerCase() == "w" ){
+        alert("go")
+    }
+})
+
+
+/* Objeto evento */
+
+var padre= document.getElementById("padre");
+
+padre.addEventListener("click", (e)=>{
+    console.log(e.target.textContent)
+    /* e.target.style.background="red" */
+
+ e.target.classList.add("color")
+})
+
+var uno= document.getElementById("uno");
+
+uno.addEventListener("click", (e)=>{
+   e.target.classList.add("color1")
+})
+
+var dos= document.getElementById("dos");
+dos.addEventListener("click", (e)=>{
+    e.target.classList.add("color2")
+})
+
+var tres= document.getElementById("tres");
+tres.addEventListener("click", (e)=>{
+   e.target.classList.add("color3")
+})
+
+
+/* CREAR ELEMENTOS DENTRO DE UN NODO O ETIQUETA */
+
+var lista= document.getElementById("lista");
+var agregar= document.getElementById("agregar")
+
+agregar.addEventListener("click", ()=>{
+ var texto= document.getElementById("texto").value;
+ /* Me trae el texto o informacion ingresado dentro del input */
+
+ var etiqueta= document.createElement("li");
+ /* CREO LA ETIQUETA */
+
+ etiqueta.textContent= texto;
+ /* ASIGNAMOS EL VALOR.DENTRO DE LA ETIQUE INSERTO EL TEXTO QUE TIENE EL INPUT */
+
+    lista.appendChild(etiqueta)
+    /* AGREGAMOS LA ETIQUETA A LA LISTA O NODO */
+
+})
